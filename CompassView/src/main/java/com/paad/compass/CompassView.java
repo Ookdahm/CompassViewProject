91f376226bcc6bbc683f07ea115a32f2c4415bfe
Tony Alexander <antalex7@gmail.com>
2013-09-17 15:44:15 -0400

    New class added

--- CompassView/src/main/java/com/paad/compass/CompassView.java
+++ CompassView/src/main/java/com/paad/compass/CompassView.java
@@ -1,14 +1,18 @@
 package com.paad.compass;
 
 import android.content.Context;
-import android.os.Bundle;
-import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
-import android.view.Menu;
 import android.view.View;
+import android.view.accessibility.AccessibilityEvent;
 
 
 public class CompassView extends View {
+
+    private float bearing;
+
 
     public CompassView (Context context){
         super(context);
@@ -25,7 +29,82 @@ public class CompassView extends View {
         initCompassView();
     }
 
+
+
+    private Paint marketPaint;
+    private Paint textPaint;
+    private Paint circlePaint;
+    private String northString;
+    private String eastString;
+    private String southString;
+    private String westString;
+    private int textHeight;
+
     protected void initCompassView(){
         setFocusable(true);
+
+        Resources r = this.getResources();
+
+        circlePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        circlePaint.setColor(r.getColor(R.color.background_color));
+        circlePaint.setStrokeWidth(1);
+        circlePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+
+        northString = r.getString(R.string.cardinal_north);
+        eastString = r.getString(R.string.cardinal_east);
+        southString = r.getString(R.string.cardinal_south);
+        westString = r.getString(R.string.cardinal_west);
+
+        textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        textPaint.setColor(r.getColor(R.color.text_color));
+
+        textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        textPaint.setColor(r.getColor(R.color.text_color));
+
+        textHeight = (int)textPaint.measureText("yY");
+
+        marketPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        marketPaint.setColor(r.getColor(R.color.market_color));
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec){
+        // The compass is a circle that fills as much space as possible.
+        // Set the measured dimensions by figuring out the shortest boundary,
+        // height or width.
+        int measuredWidth = measure(widthMeasureSpec);
+        int measuredHeight = measure(heightMeasureSpec);
+
+        int d = Math.min(measuredWidth, measuredHeight);
+
+        setMeasuredDimension(d, d);
+    }
+
+
+
+    private int measure(int measureSpec){
+        int result = 0;
+
+        // Decode the measurement specifications.
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+
+        if (specMode == MeasureSpec.UNSPECIFIED){
+            // Return a default size of 200 if no bounds are specified.
+            result = 200;
+        } else {
+            // As you want to fill the available space
+            // always return the full available bounds.
+            result=specSize;
+        }
+        return result;
+    }
+
+    public void setBearing(float _bearing){
+        bearing=_bearing;
+    }
+
+    public float getBearing(){
+        return bearing;
     }
 }
